e83d1c5f47c0a84c499a28829b7e8fb39b64ebbb
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-07-10 17:03:37 +0200

    sync

---
+++ web/demo/vote/js/howitworks.js
@@ -0,0 +1,8 @@
+[
+   {
+      "title": "",
+      "body": "",
+      "columns": [{"syntax": "js", "source": "js/vote.js", "highlights": "3-6,12"},
+                  {"syntax": "plsql", "source": "sql/pkg_vote_body.sql"}]
+   }
+]
