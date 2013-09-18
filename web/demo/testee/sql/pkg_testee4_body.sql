350354ca13c1f61e8536b18af4828ab573295fb6
Tobias Oberstein <tobias.oberstein@tavendo.de>
2013-09-18 14:34:51 +0200

    add testee package

--- web/demo/testee/sql/pkg_testee4_body.sql
+++ web/demo/testee/sql/pkg_testee4_body.sql
@@ -1,17 +1,21 @@
 CREATE OR REPLACE PACKAGE BODY pkg_testee4
 AS
 
-   PROCEDURE initiate_dispatch (topic VARCHAR2, event JSON_VALUE, options JSON)
+   FUNCTION initiate_dispatch (topic VARCHAR2, event JSON_VALUE, options JSON) RETURN NUMBER
    IS
-      l_exclude  webmq_sessionids := webmq_sessionids();
-      l_eligible  webmq_sessionids := webmq_sessionids();
-      l_cnt NUMBER;
-      l_ids JSON_LIST;
+      l_exclude   webmq_sessionids := webmq_sessionids();
+      l_eligible  webmq_sessionids := NULL;
+      l_ids       JSON_LIST;
+      l_event_id  NUMBER;
    BEGIN
+
+      -- the standard test suite WAMP testee only registers these URIs for pubsub:
       IF topic = 'http://example.com/foobar' OR SUBSTR(topic, 1, 25) = 'http://example.com/simple' THEN
 
-/*
-         IF FALSE AND options.exist('exclude') THEN
+         -- 'exlucde' always takes precedence over 'excludeMe'
+         IF options.exist('exclude') AND
+            NOT options.get('exclude').is_null AND
+            options.get('exclude').get_type = 'array' THEN
 
             l_ids := json_list(options.get('exclude'));
             FOR i IN 1..l_ids.COUNT
@@ -22,19 +26,23 @@ AS
 
          ELSE
 
-            IF options.exist('excludeMe') AND NOT options.get('excludeMe').get_bool THEN
+            -- simulate the WAMPv1 behavior (publisher is excluded by default)
+            IF options.exist('excludeMe') AND
+               NOT options.get('excludeMe').is_null AND
+               NOT options.get('excludeMe').get_bool THEN
                NULL;
             ELSE
-*/            
                l_exclude.extend();
                l_exclude(1) := options.get('me').get_string;
-               webmq.publish(topic, event, p_exclude => l_exclude);
-/*               
             END IF;
 
          END IF;
 
-         IF options.exist('eligible') THEN
+         IF options.exist('eligible') AND
+            NOT options.get('eligible').is_null AND
+            options.get('eligible').get_type = 'array' THEN
+
+            l_eligible := webmq_sessionids();
 
             l_ids := json_list(options.get('eligible'));
             FOR i IN 1..l_ids.COUNT
@@ -45,9 +53,12 @@ AS
 
          END IF;
 
-         webmq.publish(topic, event, p_exclude => l_exclude, p_eligible => l_eligible);
-*/
+         l_event_id := webmq.publish(topic, event, p_exclude => l_exclude, p_eligible => l_eligible);
+
       END IF;
+
+      RETURN l_event_id;
+
    END initiate_dispatch;
 
 END;
--- web/demo/testee/sql/pkg_testee4_spec.sql
+++ web/demo/testee/sql/pkg_testee4_spec.sql
@@ -3,11 +3,25 @@ AS
    /**
     * Copyright (c) 2013 Tavendo GmbH. Licensed under the Apache 2.0 license.
     *
-    * Testee package for use with AutobahnTestsuite: Exceptions.
+    * Testee package for use with AutobahnTestsuite: Server-initiated Events.
     *
-    * Cases: 3.6.*, 3.7.*
+    * Cases: 2.3.*, 2.4.*
     */
 
-   PROCEDURE initiate_dispatch (topic VARCHAR2, event JSON_VALUE, options JSON);
+   /**
+    * Using this procedure, the test suite will trigger a server-initiated
+    * event. Normally, server-initiated events are triggered by server-side
+    * code, but the test suite needs a control API to simulate this.
+    *
+    * @param topic: The topic to publish to.
+    * @param event: The event payload.
+    * @param options: Publication options. May have have controlling attributes:
+    *                   - 'exclude' (list of WAMP session IDs)
+    *                   - 'eligible' (list of WAMP session IDs)
+    *                   - 'excludeMe' (bool)
+    *                 and always has the informational attributes:
+    *                   - 'me' (publisher WAMP session ID).
+    */
+   FUNCTION initiate_dispatch (topic VARCHAR2, event JSON_VALUE, options JSON) RETURN NUMBER;
 END;
 /
