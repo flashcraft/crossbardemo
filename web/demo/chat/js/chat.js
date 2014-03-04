42726e83f51a963f042393fa2c9cc2438ac1e522
Alexander Goedde <alexander.goedde@tavendo.de>
2014-03-04 16:28:34 +0100

    chat demo updated to work with crossbar2

--- web/demo/chat/js/chat.js
+++ web/demo/chat/js/chat.js
@@ -1,56 +1,80 @@
-
 /******************************************************************************
  *
- *  Copyright 2012-2013 Tavendo GmbH.
+ *  Copyright 2012-2014 Tavendo GmbH.
  *
  *  Licensed under the Apache 2.0 license
  *  http://www.apache.org/licenses/LICENSE-2.0.html
  *
  ******************************************************************************/
 
-var channelBaseUri = "http://crossbar.io/crossbar/demo/chat/";
-
-var initialChannel = null;
-var presetNicks = ["Nick", "Knatterton", "Micky", "Maus", "Donald", "Bruce", "Wayne", "Clark", "Kent", "Sarah", "Connor", "Mary", "Shelley", "Rosemary", "Wilma", "Louis", "Selina", "Barbara", "Gordon", "Herbert", "The Count"];
-var nick;
-var oldNick;
-var assignedNicks = {};
-var nickColors = ["black", "orange", "green", "blue", "red"];
-
-var wsuri = get_appliance_url("hub-websocket", "ws://127.0.0.1:8080/ws");
-var sess = null;
-var retryCount = 0;
-var retryDelay = 2;
-
-var _idchars = "0123456789";
-var _idlen = 6;
-var _idpat = /^\d*$/;
-
+var channelBaseUri = "io.crossbar.demo.chat",
+    // channelBaseUri = "http://crossbar.io/crossbar/demo/chat/",
+    initialChannel = null,
+    currentSubscription = null,
+    presetNicks = ["Nick", "Knatterton", "Micky", "Maus", "Donald", "Bruce", "Wayne", "Clark", "Kent", "Sarah", "Connor", "Mary", "Shelley", "Rosemary", "Wilma", "Louis", "Selina", "Barbara", "Gordon", "Herbert", "The Count"],
+    nick,
+    oldNick,
+    assignedNicks = {},
+    nickColors = ["black", "orange", "green", "blue", "red"],
+    sess = null,
+    retryCount = 0,
+    retryDelay = 2;
 
 var oldHash = window.location.href;
+console.log("oldHash", oldHash);
 
-function isValueChar(e) {
-
-   //alert(evt);
-   var kc = e.keyCode;
-   if ((kc > 8 && kc < 46 && kc !== 32) || (kc > 90 && kc < 94) || (kc > 111 && kc < 186)) {
-      return false;
-   } else {
-      return true;
-   }
-}
+// var wsuri = get_appliance_url("hub-websocket", "ws://127.0.0.1:8080/ws");
+// var wsuri = 'ws://127.0.0.1:9000/';
+var wsuri = "ws://127.0.0.1:8080/ws";
 
 var chatWindow = null;
 
+
+updateStatusline("Not connected.");
+
+// check for controller channel id in the URL
+//var windowUrl = window.location.href; // writable reference
+var windowUrl = document.URL; // string
+
+// check for the '#' fragment
+// then cut the number after the '#'
+if (windowUrl.indexOf('#') !== -1) {
+   initialChannel = windowUrl.split('#')[1];
+} else {
+   console.log("no fragment yet");
+}
+
+setupDemo();
+
+connect();
+
+
 function switchChannel(oldChannelID, newChannelID) {
    // either oldChannelID or newChannelID could be null = start page with no demo selected
-   if (oldChannelID) {
-      sess.unsubscribe("event:" + oldChannelID);
+   if (oldChannelID && currentSubscription !== null) {
+      // sess.unsubscribe("event:" + oldChannelID);
+      currentSubscription.unsubscribe().then(
+         function() {
+            console.log("successful unsubscribe");
+         },
+         function(error) {
+            console.log("unsubscribe error ", error);
+         }
+      );
    }
 
-   sess.prefix("event", channelBaseUri);
+   // sess.prefix("event", channelBaseUri);
+   console.log("subscription uri ", channelBaseUri + "." + newChannelID);
+   sess.subscribe(channelBaseUri + "." + newChannelID, onMessage).then(
+      function(subscription) {
+         currentSubscription = subscription;
+      },
+      function(error) {
+         console.log("subscription failed ", error);
+      }
+   );
 
-   sess.subscribe("event:" + newChannelID, onMessage);
+   // sess.subscribe("event:" + newChannelID, onMessage);
 
 
    // clear messages box
@@ -65,87 +89,71 @@ function updateStatusline(status) {
 
 function connect() {
 
-   ab._Deferred = jQuery.Deferred;
+   // ab._Deferred = jQuery.Deferred;
 
-   ab.connect(wsuri,
 
-      function(session) {
-         sess = session;
-         ab.log("connected!");
-         onConnect0();
-      },
-
-      function(code, reason, detail) {
-
-         sess = null;
-         switch (code) {
-            case ab.CONNECTION_UNSUPPORTED:
-               window.location = "https://github.com/crossbario/crossbar/wiki/Browser-Support";
-               alert("Browser does not support WebSocket");
-               break;
-            case ab.CONNECTION_CLOSED:
-               window.location.reload();
-               break;
-            default:
-               ab.log(code, reason, detail);
-               retryCount = retryCount + 1;
-               updateStatusline("Connection lost. Reconnecting (" + retryCount + ") in " + retryDelay + " secs ..");
-
-               break;
-         }
-      },
-
-      { 'maxRetries': 60, 'retryDelay': 2000 }
+   var connection = new autobahn.Connection({
+      url: wsuri,
+      realm: 'realm1'}
    );
+
+   connection.onopen = function (session) {
+      sess = session;
+
+      updateStatusline("Connected to " + wsuri);
+
+      // if window url contained channel id, do the subscriptions for this
+      if (initialChannel) {
+         switchChannel(null, initialChannel);
+      }
+
+      // set initial hash value/channel
+      location.hash = ""; // reset in case of reload with hash "#1"
+      location.hash = "#ch1";
+
+
+      sess.subscribe("com.test.topic", function(){console.log("test text received ", arguments);}).then(function(){console.log("test subscribed", arguments);}, function(){console.log("test subscribe failed", arguments);});
+      // sess.publish("com.test.topic", ["foobar"]);
+   };
+
+   connection.onclose = function() {
+      console.log("connection closed ", arguments);
+   }
+
+   connection.open();
+
 }
 
 
-function onConnect0() {
-   sess.authreq().then(function() {
-      sess.auth().then(onAuth, ab.log);
-   }, ab.log);
-}
+// function onConnect0() {
+//    sess.authreq().then(function() {
+//       sess.auth().then(onAuth, console.log);
+//    }, console.log);
+// }
 
 
-function onAuth(permissions) {
-   //ab.log("authenticated!", permissions);
+// function onAuth(permissions) {
+//    //console.log("authenticated!", permissions);
 
-   updateStatusline("Connected to " + wsuri);
-   retryCount = 0;
+//    updateStatusline("Connected to " + wsuri);
+//    retryCount = 0;
 
-   // if window url contained channel id, do the subscriptions for this
-   if (initialChannel) {
-      switchChannel(null, initialChannel);
-   }
+//    // if window url contained channel id, do the subscriptions for this
+//    if (initialChannel) {
+//       switchChannel(null, initialChannel);
+//    }
 
-   // set initial hash value/channel
-   location.hash = ""; // reset in case of reload with hash "#1"
-   location.hash = "#1";
-
-
-};
+//    // set initial hash value/channel
+//    location.hash = ""; // reset in case of reload with hash "#1"
+//    location.hash = "#1";
 
 
-$(document).ready(function() {
-   updateStatusline("Not connected.");
+// };
 
-   // check for controller channel id in the URL
-   //var windowUrl = window.location.href; // writable reference
-   var windowUrl = document.URL; // string
 
-   // check for the '#' fragment
-   // then cut the number after the '#'
-   if (windowUrl.indexOf('#') !== -1) {
-      initialChannel = windowUrl.split('#')[1];
-   }
-
-   setupDemo();
-
-   connect();
-});
 
 function onHashChanged(evt) {
-   //examineMe = evt;
+   console.log("onHashChanged", oldHash);
 
    //var newUrl = evt.newURL; // IE event does not contain this value
    //var oldUrl = evt.oldURL; // IE event does not contain this value
@@ -177,7 +185,7 @@ function onHashChanged(evt) {
 
    for (var i = 0; i < channelSelectors.length; i++) {
 
-      if (i + 1 == newChannelID) {
+      if ("ch" + (i + 1) == newChannelID) {
 
          $(channelSelectors[i]).addClass("channel_selected");
          //channelSelectors[i].classList.add("channel_selected");
@@ -199,15 +207,13 @@ function onHashChanged(evt) {
 
 function setupDemo() {
 
-   ab.log("stuff to set up");
+   // console.log("stuff to set up");
 
    chatWindow = $("#chat_window");
 
+
    // add 'onhashchange' event to trigger the channel change + chat window display
    window.onhashchange = onHashChanged;
-
-   //// initial hide of chat window
-   //$("#chat_window").toggle()
 
    // initial timed display of the chat window
    window.setTimeout(function() { $("#chat_window").toggle(300) }, 2300);
@@ -218,12 +224,13 @@ function setupDemo() {
       var messagesBox = $("#messages_box")[0];
       // scroll messages box
       messagesBox.scrollTop = messagesBox.scrollHeight;
-
    };
+
    document.getElementById("hide_chat_window").onclick = function () {
       $("#chat_window").toggle(300);
       $("#show_chat_window").removeClass("message_received");
    };
+
 
    // set random preset nick
    nick = $("#nick");
@@ -234,19 +241,18 @@ function setupDemo() {
 
    // set 'enter' on chat message textarea sends message
    messageInput = $("#message_input")[0];
-   //examineMe = messageInput;
-   //messageInput.addEventListener('keypress', function(e) {
    messageInput.onkeypress = function(e) {
       var e = e || event; // IE8 fix, since here the window.event, not the event itself contains the keyCode
-      //ab.log("keypressed", e);
+      //console.log("keypressed", e);
       if (e.keyCode === 13) {
-         ab.log("enter");
+         console.log("enter");
          sendMessage(messageInput);
          //e.preventDefault(); // older IEs don't do this
          return false;
       };
 
    };
+
 
    $("#helpButton").click(function() {
       $(".info_bar").toggle();
@@ -269,11 +275,14 @@ function sendMessage(messageInput) {
    if (windowUrl.indexOf('#') !== -1) {
       channel = windowUrl.split('#')[1];
    }
-   // ab.log("to send:", message, currentNick, channel);
+   // console.log("to send:", message, currentNick, channel);
    var payload = {};
    payload.message = message;
    payload.nick = currentNick;
-   sess.publish("event:" + channel, payload, true);
+
+   // sess.publish("event:" + channel, payload, true);
+   console.log("publishing to ", channelBaseUri + "." + channel);
+   sess.publish(channelBaseUri + "." + channel, [payload], {}, {exclude_me: false});
 
    // clear the message input
    messageInput.value = '';
@@ -326,17 +335,16 @@ function getNickColor(nickString) {
 };
 
 
-function onMessage(topicUri, event) {
+// function onMessage(topicUri, event) {
+function onMessage(args, kwargs, details) {
 
-   console.log(topicUri);
-   console.log(event);
-
-   addMessage(event);
+   addMessage(args[0]);
    // set new message highlighting on "chat" button
    $("#show_chat_window").addClass("message_received");
 }
 
 function addMessage(payload) {
+   console.log("message received", payload);
 
    // get the parts of the message event
    var message = payload.message;
