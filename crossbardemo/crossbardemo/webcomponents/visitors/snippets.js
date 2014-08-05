238d385573ce1ab2d1bc52f2a3e81a280e7d5562
Alexander Goedde <alexander.goedde@tavendo.de>
2014-08-05 17:35:22 +0200

    re-added sublime project, started webcomponent visitor widget

---
+++ crossbardemo/crossbardemo/webcomponents/visitors/snippets.js
@@ -0,0 +1,14 @@
+<x-foo></x-foo>
+
+
+var xFoo = document.createElement('x-foo');
+xFoo.addEventListener('click', function(e) {
+  alert('Thanks!');
+});
+
+var xFoo = new XFoo();
+document.body.appendChild(xFoo);
+
+------------------------------
+
+
