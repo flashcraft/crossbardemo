cd772df8ee82d3ff2ac65ca5c468e9d75d7466a3
goeddea <alexander.goedde@tavendo.de>
2014-10-14 15:45:13 +0200

    removed create test event code from editform - progress on #23

--- crossbardemo/crossbardemo/web/demo/form/knockout/editform_node/web/js/editform.js
+++ crossbardemo/crossbardemo/web/demo/form/knockout/editform_node/web/js/editform.js
@@ -92,6 +92,7 @@ function fillList (res) {
 
    // result list may be empty
    if(res === null || res.length === null) {
+      // needs some proper error handling
       return;
    }
 
@@ -235,7 +236,6 @@ function ViewModel () {
    this.listData = ko.observableArray([]).indexed('index');
 
    // details editable observables
-
    self.detailsEditable = {
       "index": ko.observable(),
       "orderNumber": ko.observable(),
@@ -649,75 +649,3 @@ function listItem (data) {
       itemState: ko.observable()
    };
 }
-
-function addTestItems (numberOfItemsToAdd) {
-
-   for ( var i = 0; i < numberOfItemsToAdd + 1; i++ ) {
-      // create item to send
-      var saveSet = { inStock: i, name: "Test Item " + i, orderNumber: "TT-" + i, price: i*3.5, size: i +4, weight: i*1.3+40 };
-      session.call("form:create", [], saveSet).then(
-         function(res) {
-            session.log("saved", res);
-         },
-         session.log
-      );
-   }
-}
-
-var oracleForm = {};
-
-oracleForm.testEvents = function(evt, rep) {
-   switch(evt) {
-      case "create":
-         rep = !rep ? 1 : rep;
-         for( var i = 0; i < rep; i++) {
-            console.log("constructing", i);
-            var saveSet = { inStock: i, name: "Test Item " + i, orderNumber: "TT-" + i, price: i*3.5, size: i +4, weight: i*1.3+40 };
-            vm.normalizeSet(saveSet);
-            session.call("form:create", [], saveSet).then(
-               function (res) {
-                  session.log("created", res);
-               },
-               session.log
-            );
-         }
-         break;
-      case "delete":
-         // console.log("no deleting yet");
-         var items;
-         session.call("form:filter", [rep], {name: {type: "prefix", value: "Test" }}).then(function(res) {
-            for(var i = 0; i < rep; i++) {
-               if(res[i]) {
-                  // var index = getIndexFromId(res[i].id);
-                  console.log("id ", res[i].id);
-                  session.call("form:delete", [res[i].id]).then(
-                     function(res) {
-                        session.log("deleted", res);
-                        // delete the item
-                        onItemDeleted("localDelete", res[i].id);
-                     },
-                     session.log
-                  );
-               }
-            }
-         }, session.log);
-
-         break;
-      case "update":
-         // console.log("no update yet");
-         var items;
-         session.call("form:filter", [rep], {name: {type: "prefix", value: "Test" }}).then(function(res) {
-            for(var i = 0; i < rep; i++) {
-               if(res[i]) {
-                  // var index = getIndexFromId(res[i].id);
-                  console.log("id ", res[i].id);
-                  var updated = { id: res[i].id, orderNumber: "updated " + i*rep};
-                  session.call("form:update", [], updated).then(session.log, session.log);
-               }
-            }
-         }, session.log);
-         break;
-      default:
-         break;
-   }
-};
