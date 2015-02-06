7cd3ab44fd2fad4090eacf1b01a9017fcd25c960
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-02-06 17:40:58 +0100

    activate votes demo; bump version

--- crossbardemo/crossbardemo/__init__.py
+++ crossbardemo/crossbardemo/__init__.py
@@ -1,9 +1,9 @@
 ######################################################################
 ##
-##   Copyright (C) 2013-2014 Tavendo GmbH.
+##   Copyright (C) 2013-2015 Tavendo GmbH.
 ##   Licensed under Apache 2.0 license
 ##   See: http://www.apache.org/licenses/LICENSE-2.0.html
 ##
 ######################################################################
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
